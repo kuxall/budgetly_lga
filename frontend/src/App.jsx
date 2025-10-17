b7d67d39539f77d9bdca35b620e964ec4415afda
rhythm <rhsharma0310@gmail.com>
2025-10-17 13:39:56 -0400

    changed app js to jsx, added page for profile settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Settings from "./pages/Settings/Settings";
 
 function App() {
   return (
@@ -11,10 +12,13 @@ function App() {
           path="/"
           element={
             <div className="flex items-center justify-center h-screen bg-gray-100">
-              <h1 className="text-3xl font-bold text-blue-600">Welcome to Budgetly!</h1>
+              <h1 className="text-3xl font-bold text-blue-600">
+                Welcome to Budgetly!
+              </h1>
             </div>
           }
         />
+        <Route path="settings" element={<Settings />} />
       </Routes>
     </Router>
   );
